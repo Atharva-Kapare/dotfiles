5872ef0df492f025c0cdd2db97907cebb9d947e2
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-06-07 18:49:52 +1000

    init home manager flake

---
+++ home-manager/home.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "ak";
+  home.homeDirectory = "/home/ak";
+
+
+  home.packages = with pkgs; [
+    btop
+    git
+    neovim
+    thefuck
+    starship
+
+    alacritty
+
+    spotify
+
+  ];
+
+  programs.starship.enable = true;
+
+
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+}
