65089ad42f7bd18bc9680ca78745851b32903bfb
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-06-29 22:35:11 +1000

    format on save

--- programs/nvim/default.nix
+++ programs/nvim/default.nix
@@ -1,53 +1,57 @@
-{ config, pkgs, inputs, ...}: {
-    # Configuration stuff ...
-    imports = [
-        inputs.nixvim.homeManagerModules.nixvim # DO NOT REMOVE
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  # Configuration stuff ...
+  imports = [
+    inputs.nixvim.homeManagerModules.nixvim # DO NOT REMOVE
 
-	# ./plugins/hardtime.nix
-	./plugins/which-key.nix
-	./plugins/lualine.nix
-	./plugins/telescope.nix
-	./plugins/toggleterm.nix
-	./plugins/treesitter.nix
-	./plugins/lsp.nix
+    # ./plugins/hardtime.nix
+    ./plugins/which-key.nix
+    ./plugins/lualine.nix
+    ./plugins/telescope.nix
+    ./plugins/toggleterm.nix
+    ./plugins/treesitter.nix
+    ./plugins/lsp.nix
+  ];
 
+  programs.nixvim = {
+    enable = true;
+    vimAlias = true;
+    viAlias = true;
+    defaultEditor = true;
+
+    globals.mapleader = " ";
+
+    keymaps = [
+      {
+        key = "<C-h>";
+        action = "<Cmd>wincmd h<CR>";
+      }
+      {
+        key = "<C-j>";
+        action = "<Cmd>wincmd j<CR>";
+      }
+      {
+        key = "<C-k>";
+        action = "<Cmd>wincmd k<CR>";
+      }
+      {
+        key = "<C-l>";
+        action = "<Cmd>wincmd l<CR>";
+      }
     ];
-    
-    programs.nixvim = {
-        enable = true;
-        vimAlias = true;
-	    viAlias = true;
-	    defaultEditor = true;
 
-	    globals.mapleader = " ";
-
-        keymaps = [
-            {
-                key = "<C-h>";
-                action = "<Cmd>wincmd h<CR>";
-            } 
-            {
-                key = "<C-j>";
-                action = "<Cmd>wincmd j<CR>";
-            } 
-            {
-                key = "<C-k>";
-                action = "<Cmd>wincmd k<CR>";
-            } 
-            {
-                key = "<C-l>";
-                action = "<Cmd>wincmd l<CR>";
-            } 
-        ];
-
-        # Configure neovim options...
-        opts = {
-            relativenumber = true;
-            incsearch = true;
-            expandtab = true;
-            shiftwidth = 4;
-            smartindent = true;
-            tabstop = 4;
-        };
+    # Configure neovim options...
+    opts = {
+      relativenumber = true;
+      incsearch = true;
+      expandtab = true;
+      shiftwidth = 4;
+      smartindent = true;
+      tabstop = 4;
     };
+  };
 }
