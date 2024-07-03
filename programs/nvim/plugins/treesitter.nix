1d869d92fc43324648eccf244776b1e50edc50a6
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-07-03 22:09:13 +1000

    fixed some treesitter things

--- programs/nvim/plugins/treesitter.nix
+++ programs/nvim/plugins/treesitter.nix
@@ -1,19 +1,31 @@
-{ config, pkgs, inputs, ...}: {
-# Configuration stuff ...
-    imports = [
-    ];
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  # Configuration stuff ...
+  imports = [
+  ];
 
-    programs.nixvim.plugins = {
-        treesitter = {
-            enable = true;
-            nixGrammars = true;
-            indent = true;
-        };
-        treesitter-context = {
-            enable = true;
-            settings = { max_lines = 2; };
-        };
-        rainbow-delimiters.enable = true;
+  programs.nixvim.plugins = {
+    treesitter = {
+      enable = true;
+      nixGrammars = true;
+      indent = true;
+      incrementalSelection.enable = true;
     };
-}
+    treesitter-context = {
+      enable = true;
+      settings = {max_lines = 2;};
+    };
+    treesitter-refactor = {
+      enable = true;
 
+      navigation = {
+        enable = true;
+      };
+    };
+    rainbow-delimiters.enable = true;
+  };
+}
