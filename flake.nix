cd5e0bbb3b66f645af8e51b82376b23f9492914b
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-07-18 20:24:20 +1000

    moved to the profiles

--- flake.nix
+++ flake.nix
@@ -1,56 +1,55 @@
 {
   description = "Home Manager configuration of ak";
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs :
-    let
-
-      systemSettings = {
-        system = "x86_64-linux"; # system arch
-        hostname = "white-dwarf"; # hostname
-      };
-
-      userSettings = rec {
-        username = "ak";
-        email = "atharva.s.kapare@gmail.com"; #Used by some configurations, like git
-      };
-
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-
-    in {
-
-      # imports = [
-      #   ./profiles/personal # (2)
-      # ];
-      
-      homeConfigurations = { 
-	      ak = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = {
-            inherit userSettings;
-            inherit inputs;
-          };
-          modules = [ ./profiles/personal/home.nix ];
-        };
-      };
-
-      nixosConfigurations = {
-        white-dwarf = nixpkgs.lib.nixosSystem {
-          system = systemSettings.system;
-          modules = [
-            ./configuration.nix
-          ]; # load configuration.nix from selected PROFILE
-          specialArgs = {
-            # pass config variables from above
-            inherit pkgs;
-            inherit systemSettings;
-            inherit userSettings;
-            inherit inputs;
-          };
-        };
-      };
-
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    systemSettings = {
+      system = "x86_64-linux"; # system arch
+      hostname = "white-dwarf"; # hostname
+      profile = "personal";
     };
+
+    userSettings = rec {
+      username = "ak";
+      email = "atharva.s.kapare@gmail.com"; #Used by some configurations, like git
+    };
+
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    # imports = [
+    #   ./profiles/personal # (2)
+    # ];
+
+    homeConfigurations = {
+      ${userSettings.username} = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+          inherit userSettings;
+          inherit inputs;
+        };
+        modules = [./profiles/${systemSettings.profile}/home.nix];
+      };
+    };
+
+    nixosConfigurations = {
+      ${systemSettings.hostname} = nixpkgs.lib.nixosSystem {
+        system = systemSettings.system;
+        modules = [./profiles/${systemSettings.profile}/configuration.nix];
+        specialArgs = {
+          # pass config variables from above
+          inherit pkgs;
+          inherit systemSettings;
+          inherit userSettings;
+          inherit inputs;
+        };
+      };
+    };
+  };
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
@@ -62,9 +61,9 @@
     };
 
     nixvim = {
-        url = "github:nix-community/nixvim";
-        # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
-        inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/nixvim";
+      # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 }
