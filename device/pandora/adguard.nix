a65e80a4e8f69376ad976e29da474b625d20ef69
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-07-19 21:57:38 +1000

    test

--- device/pandora/adguard.nix
+++ device/pandora/adguard.nix
@@ -9,25 +9,34 @@ in {
   #   adguardhome # adguardhome
   # ];
 
-  networking = {
-    firewall = {
-      allowedTCPPorts = [ adguardPort ];
-      allowedUDPPorts = [ 53 ];
-    };
+  # networking = {
+  #   firewall = {
+  #     allowedTCPPorts = [ adguardPort ];
+  #     allowedUDPPorts = [ 53 ];
+  #   };
+  # };
+
+  # services = {
+  #   adguardhome = {
+  #     enable = true;
+  #     openFirewall = true;
+  #     allowDHCP = true;
+  #     port = adguardPort;
+
+  #     settings = {
+  #       # https://github.com/NixOS/nixpkgs/issues/246461
+  #       schema_version = 20;
+  #     };
+
+  #   };
+  # };
+
+    services.adguardhome = {
+    enable = true;
+    host = "0.0.0.0";
+    port = 3000;
+    allowDHCP = true;
+    openFirewall = true;
   };
-
-  services = {
-    adguardhome = {
-      enable = true;
-      openFirewall = true;
-      allowDHCP = true;
-      port = adguardPort;
-
-      settings = {
-        # https://github.com/NixOS/nixpkgs/issues/246461
-        schema_version = 20;
-      };
-
-    };
-  };
+  networking.firewall.allowedTCPPorts = [ 53 3000 3001 ];
 }
