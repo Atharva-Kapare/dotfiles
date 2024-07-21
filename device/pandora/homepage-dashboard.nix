de8dab1bbeaa718948a20ba394785ad5abff696e
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-07-21 23:30:30 +1000

    bookmarks and widgets

--- device/pandora/homepage-dashboard.nix
+++ device/pandora/homepage-dashboard.nix
@@ -13,6 +13,78 @@
       enable = true;
       openFirewall = true;
       listenPort = 8082;
+
+      widgets = [
+        {
+          resources = {
+            cpu = true;
+            disk = "/";
+            memory = true;
+          };
+        }
+        {
+          search = {
+            provider = "duckduckgo";
+            target = "_blank";
+          };
+        }
+      ];
+
+      bookmarks = [
+        {
+          Developer = [
+            {
+              Github = [
+                {""
+                  abbr = "GH";
+                  href = "https://github.com/";
+                }
+              ];
+            }
+          ];
+        }
+        {
+          Entertainment = [
+            {
+              YouTube = [
+                {
+                  abbr = "YT";
+                  href = "https://youtube.com/";
+                }
+              ];
+            }
+          ];
+        }
+      ];
+
+      services = [
+        {
+          "Monitoring" = [
+            {
+              "Cockpit" = {
+                description = "Cockpit for system monitoring";
+                href = "http://cockpit.home.io/";
+              };
+            }
+            {
+              "AdGuardHome" = {
+                description = "Adguard for network monitoring";
+                href = "http://adguard.home.io/";
+              };
+            }
+          ];
+        }
+        {
+          "Media" = [
+            {
+              "Jellyfin" = {
+                description = "Homepage is the best";
+                href = "http://localhost/";
+              };
+            }
+          ];
+        }
+      ];
       
     };
 
