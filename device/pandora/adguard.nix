19eaf27a9a874a42c84535c52ed2f148a0c2f781
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-07-21 21:00:18 +1000

    again again

--- device/pandora/adguard.nix
+++ device/pandora/adguard.nix
@@ -24,10 +24,10 @@ in {
 
     };
 
-    nginx.virtualHosts."adguard.my.home" = {
+    nginx.virtualHosts."my.home.io" = {
       # addSSL = true;
       # enableACME = true;
-      locations."/" = {
+      locations."/adguard" = {
         proxyPass = "http://127.0.0.1:3000";
         proxyWebsockets = true;
       };
