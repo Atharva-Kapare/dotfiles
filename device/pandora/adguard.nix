35493b174fa81f168375d4b35e5dccc036479cdd
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-07-21 20:31:10 +1000

    added the actual location

--- device/pandora/adguard.nix
+++ device/pandora/adguard.nix
@@ -28,7 +28,7 @@ in {
       # addSSL = true;
       # enableACME = true;
       locations."/" = {
-        proxyPass = "http://$1:port/your/location";
+        proxyPass = "http://127.0.0.1:3000";
         proxyWebsockets = true;
       };
     };
