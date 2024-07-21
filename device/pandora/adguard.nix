ec825556cbd9dce7f1e2aac2617331e1a05b932c
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-07-21 15:22:25 +1000

    switched to podman

--- device/pandora/adguard.nix
+++ device/pandora/adguard.nix
@@ -29,47 +29,47 @@ in {
   #   };
   # };
 
-  services.adguardhome = {
-    enable = true;
-    host = "0.0.0.0";
-    port = adguardPort;
-    allowDHCP = true;
-    openFirewall = true;
-  };
-
-  # virtualisation.oci-containers.containers.adguard = {
-  #   hostname = "adguard";
-  #   autoStart = true;
-  #   image = "adguard/adguardhome";
-  #   ports = [
-  #     # DNS server
-  #     "53:53/tcp"
-  #     "53:53/udp"
-  #     # "853:853/tcp" # DNS over TLS
-  #     # "853:853/udp" # DNS over QUIC
-
-  #     # DHCP server
-  #     # "67:67/udp"
-  #     # "68:68/tcp"
-  #     # "68:68/udp"
-
-  #     # Admin panel + DNS over HTTPS
-  #     # "80:80/tcp"
-  #     # "443:443/tcp"
-  #     # "443:443/udp"
-  #     "3000:3000/tcp" # Web interface
-
-  #     # DNSCrypt
-  #     # "5443:5443/tcp"
-  #     # "5443:5443/udp"
-
-  #     # "6060:6060/tcp" # Debugging
-  #   ];
-  #   volumes = [
-  #     "adguard_config:/opt/adguardhome/conf"
-  #     "adguard_work:/opt/adguardhome/work"
-  #   ];
+  # services.adguardhome = {
+  #   enable = true;
+  #   host = "0.0.0.0";
+  #   port = adguardPort;
+  #   allowDHCP = true;
+  #   openFirewall = true;
   # };
 
-  networking.firewall.allowedTCPPorts = [ 53 3000 ];
+  virtualisation.oci-containers.containers.adguard = {
+    hostname = "adguard";
+    autoStart = true;
+    image = "adguard/adguardhome";
+    ports = [
+      # DNS server
+      "53:53/tcp"
+      "53:53/udp"
+      # "853:853/tcp" # DNS over TLS
+      # "853:853/udp" # DNS over QUIC
+
+      # DHCP server
+      # "67:67/udp"
+      # "68:68/tcp"
+      # "68:68/udp"
+
+      # Admin panel + DNS over HTTPS
+      "80:80/tcp"
+      "443:443/tcp"
+      "443:443/udp"
+      "3000:3000/tcp" # Web interface
+
+      # DNSCrypt
+      # "5443:5443/tcp"
+      # "5443:5443/udp"
+
+      # "6060:6060/tcp" # Debugging
+    ];
+    volumes = [
+      "adguard_config:/opt/adguardhome/conf"
+      "adguard_work:/opt/adguardhome/work"
+    ];
+  };
+
+  networking.firewall.allowedTCPPorts = [ 80 53 443 3000 ];
 }
