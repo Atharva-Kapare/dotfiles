3165807a154a4d62278905f95dc9660ccfc2e7cf
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-07-21 20:35:01 +1000

    cockpit proxy

--- device/pandora/cockpit.nix
+++ device/pandora/cockpit.nix
@@ -8,13 +8,22 @@
      cockpit # cockpit
   ];
 
-    services.cockpit = {
+  services.cockpit = {
     enable = true;
     port = 9090;
     openFirewall = true;
     settings = {
       WebService = {
         AllowUnencrypted = true;
+      };
+    };
+
+    nginx.virtualHosts."cockpit.home" = {
+      # addSSL = true;
+      # enableACME = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:9090";
+        proxyWebsockets = true;
       };
     };
   };
