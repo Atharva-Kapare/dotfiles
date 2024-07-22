003258d193e1267b0417577542cdd7448ac3bffe
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-07-22 11:29:57 +1000

    reverted a lot to see what would happen

--- device/pandora/home-assistant/default.nix
+++ device/pandora/home-assistant/default.nix
@@ -11,30 +11,30 @@
     home-assistant = {
       enable = true;
       openFirewall = true;
-      configWritable = true;
+      # configWritable = true;
       
-      config = {
-        homeassistant = {
-          name = "Home";
-          time_zone = "Australia/Sydney";
-          latitude = -33.0;
-          longitude = 151.0;
-          elevation = 1.0;
-          unit_system = "metric";
-          temperature_unit = "C";
-        };
-        # Enable the frontend
-        frontend = { };
-        mobile_app = { };
+      # config = {
+      #   homeassistant = {
+      #     name = "Home";
+      #     time_zone = "Australia/Sydney";
+      #     latitude = -33.0;
+      #     longitude = 151.0;
+      #     elevation = 1.0;
+      #     unit_system = "metric";
+      #     temperature_unit = "C";
+      #   };
+      #   # Enable the frontend
+      #   frontend = { };
+      #   mobile_app = { };
 
-        "automation ui" = "!include automations.yaml";
-      };
+      #   "automation ui" = "!include automations.yaml";
+      # };
 
-      extraComponents = [
-        "default_config"
-        "esphome"
-        "zha"
-      ];
+      # extraComponents = [
+      #   "default_config"
+      #   "esphome"
+      #   "zha"
+      # ];
       
     };
 
