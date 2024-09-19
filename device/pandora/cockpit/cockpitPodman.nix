55b2adb45d72750863f45eaad5eca41266dc4b82
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-09-19 22:13:22 +1000

    version change

--- device/pandora/cockpit/cockpitPodman.nix
+++ device/pandora/cockpit/cockpitPodman.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "cockpit-podman";
-  version = "81";
+  version = "94";
 
   src = fetchzip {
     url = "https://github.com/cockpit-project/${pname}/releases/download/${version}/${pname}-${version}.tar.xz";
