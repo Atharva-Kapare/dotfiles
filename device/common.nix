0d249d9a49551b5ac0c27d3b073d64e7e57ebdae
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-09-20 17:47:35 +1000

    removed xserver from common

--- device/common.nix
+++ device/common.nix
@@ -53,17 +53,17 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   # services.xserver.displayManager.gdm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+  # services.xserver.xkb = {
+  #   layout = "us";
+  #   variant = "";
+  # };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
