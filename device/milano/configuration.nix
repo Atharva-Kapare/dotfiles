a43a4403b12d6a0e0d7d5cd24152ebf11171c3b2
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-09-20 17:57:40 +1000

    test

---
+++ device/milano/configuration.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  pkgs,
+  systemSettings,
+  userSettings,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    # ./hardware-configuration.nix
+    /etc/nixos/hardware-configuration.nix
+    ../common.nix
+
+    ./cockpit
+  ];
+
+  # Useful other development tools
+  environment.systemPackages = with pkgs; [
+
+  ];
+
+  services.nginx = {
+    enable = true;
+
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+  };
+
+  services.openssh.enable = true;
+
+  # security.acme.defaults.email = userSettings.email;
+  # security.acme.acceptTerms = true;
+}
