b25075086c31b44bee64d56da04811d6ef971606
Atharva-Kapare <atharva.s.kapare@gmail.com>
2024-09-22 14:41:42 +1000

    jellyseerr

--- device/milano/jellyfin.nix
+++ device/milano/jellyfin.nix
@@ -8,16 +8,23 @@
      
   ];
 
-  services.jellyfin = {
-    enable = true;
-    openFirewall = true;
-
-  };
+  services = {
 
     # 8096/tcp is used by default for HTTP traffic. You can change this in the dashboard.
     # 8920/tcp is used by default for HTTPS traffic. You can change this in the dashboard.
     # 1900/udp is used for service auto-discovery. This is not configurable.
     # 7359/udp is also used for auto-discovery. This is not configurable.
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
+    };
 
+    jellyseerr = {
+      enable = true;
+      port = 5055;
+      openFirewall = true;
+    }
+  };
 
+  
 }
