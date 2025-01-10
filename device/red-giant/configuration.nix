1ba50bf83074d616f6ec9103a5edb9f88fb9194d
Atharva-Kapare <atharva.s.kapare@gmail.com>
2025-01-10 14:17:52 +1100

    Feat: Added steam

--- device/red-giant/configuration.nix
+++ device/red-giant/configuration.nix
@@ -15,6 +15,13 @@
     ../common.nix
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
