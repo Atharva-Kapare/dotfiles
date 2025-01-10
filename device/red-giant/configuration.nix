8738b8c02183f3e0eb4cd3b466f59b7380512e0c
Atharva-Kapare <atharva.s.kapare@gmail.com>
2025-01-10 13:30:11 +1100

    Fix: enabled modesetting for nvidia. Required according to docs

--- device/red-giant/configuration.nix
+++ device/red-giant/configuration.nix
@@ -28,6 +28,7 @@
       # Enable the Nvidia settings menu,
       # accessible via `nvidia-settings`.
       nvidiaSettings = true;
+      modesetting.enable = true;
 
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
       package = config.boot.kernelPackages.nvidiaPackages.production;
